b4ccce6da53193f4be975e8fd92407012eb75e92
rei <collapse1227@gmail.com>
2019-03-14 13:19:56 +0900

    [add]ボタンと外見の作成 外見の作成とボタンの配置を行いました。

--- Calc.java
+++ Calc.java
@@ -1,7 +1,103 @@
 package Calc;
 
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
 
 public class Calc extends JFrame {
+	private static final long serialVersionUID = 1L;
 
+	JPanel contentPane = new JPanel();
+	BorderLayout borderLayout1 = new BorderLayout();
+	JTextField result = new JTextField(""); //計算結果を表示するテキストフィールド
+	double stackedValue = 0.0; //演算子ボタンを押す前にテキストフィールドにあった値
+	boolean isStacked = false; //stackedValueに数値を入力したかどうか
+	boolean afterCalc = false; //演算子ボタンを押した後かどうか
+	String currentOp = ""; //押された演算子ボタンの名前
+
+	//フレームのビルド
+	public Calc() {
+		contentPane.setLayout(borderLayout1);
+		this.setSize(new Dimension(250, 300));
+		this.setTitle("電子式卓上計算機");
+		this.setContentPane(contentPane);
+
+		contentPane.add(result, BorderLayout.NORTH); //テキストフィールドを配置
+
+		JPanel keyPanel = new JPanel(); //ボタンを配置するパネルを用意
+		keyPanel.setLayout(new GridLayout(4, 4)); //4行4列のGridLayoutにする
+		contentPane.add(keyPanel, BorderLayout.CENTER);
+
+		keyPanel.add(new NumberButton("7"), 0); //ボタンをレイアウトにはめこんでいく
+		keyPanel.add(new NumberButton("8"), 1);
+		keyPanel.add(new NumberButton("9"), 2);
+		keyPanel.add(new CalcButton("÷"), 3);
+		keyPanel.add(new NumberButton("4"), 4);
+		keyPanel.add(new NumberButton("5"), 5);
+		keyPanel.add(new NumberButton("6"), 6);
+		keyPanel.add(new CalcButton("×"), 7);
+		keyPanel.add(new NumberButton("1"), 8);
+		keyPanel.add(new NumberButton("2"), 9);
+		keyPanel.add(new NumberButton("3"), 10);
+		keyPanel.add(new CalcButton("－"), 11);
+		keyPanel.add(new NumberButton("0"), 12);
+		keyPanel.add(new NumberButton("."), 13);
+		keyPanel.add(new CalcButton("＋"), 14);
+		keyPanel.add(new CalcButton("＝"), 15);
+
+		contentPane.add(new ClearButton(), BorderLayout.SOUTH);//Cボタンを配置する
+		this.setVisible(true);
+	}
+
+	/* テキストフィールドに引数の文字列をつなげる */
+	public void appendResult(String c) {
+		if (!afterCalc) //演算子ボタンを押した直後でないなら
+			result.setText(result.getText() + c); //押したボタンの名前をつなげる
+		else {
+			result.setText(c); //押したボタンの文字列だけを設定する（いったんクリアしたかに見える）
+			afterCalc = false;
+		}
+	}
+
+	/* 数字を入力するボタンの定義 */
+	public class NumberButton extends JButton implements ActionListener {
+		private static final long serialVersionUID = 1L;
+
+		public NumberButton(String keyTop) {
+		}
+
+		public void actionPerformed(ActionEvent evt) {
+		}
+	}
+
+	/* 演算子ボタンを定義 */
+	public class CalcButton extends JButton implements ActionListener {
+		private static final long serialVersionUID = 1L;
+
+		public CalcButton(String op) {
+		}
+
+		public void actionPerformed(ActionEvent e) {
+		}
+	}
+
+	/* クリアボタンの定義 */
+	public class ClearButton extends JButton implements ActionListener {
+
+		private static final long serialVersionUID = 1L;
+
+		public ClearButton() {
+		}
+
+		public void actionPerformed(ActionEvent evt) {
+		}
+	}
 }
